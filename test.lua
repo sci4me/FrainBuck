eb890ee6840ebe21b6b9f4debd3ce09100497d98
sci4me <minecraftman7778@gmail.com>
2017-08-04 14:18:20 -0500

    print some fibonacci numbers

--- test.lua
+++ test.lua
@@ -291,11 +291,33 @@ end
 
 local a, b = alloc(2)
 
-set(a, 255)
+set(a, 1)
+set(b, 1)
 
+local i = alloc()
+
+local tmp1, tmp2, n = alloc(3)
+
+printCell(a)
+printCell(b)
+
+set(i, 11)
 open()
-	copy(a, b)
-	printCell(b)
-	to(a)
+	copy(a, tmp1)
+	copy(b, tmp2)
+	open()
+		sub()
+		to(tmp1)
+		add()
+		to(tmp2)
+	close()	
+
+	copy(tmp1, n)
+	printCell(n)
+
+	move(b, a)
+	move(tmp1, b)
+	
+	to(i)
 	sub()
 close()
