9d34e68b917922e3f0009503ad49c6258e4e8220
sci4me <minecraftman7778@gmail.com>
2017-08-06 05:34:00 -0500

    go back to the array example

--- test.lua
+++ test.lua
@@ -4,43 +4,20 @@ include("src/math.lua")
 include("src/print.lua")
 include("src/array.lua")
 
-local ADD = 1
-local SUB = 2
-local LEFT = 3
-local RIGHT = 4
-local READ = 5
-local WRITE = 7
-local OPEN = 8
-local CLOSE = 9
+local i, v = alloc(2)
 
-local r, ip, dp, i, tmp, tmp2 = alloc(6)
+local a = allocBlock(9)
 
-set(r, 1)
+ainit(a)
 
-local code = allocBlock(256 * 2 + 3)
-local data = allocBlock(256 * 2 + 3)
-ainit(code)
-ainit(data)
-
--- fill out code
-local bfcode = {
-	ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-	ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-	ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-	ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-	ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD, ADD,
-	ADD, WRITE, WRITE, WRITE
-}
-for i = 1, #bfcode do
-	set(tmp, i - 1)
-	set(tmp2, bfcode[i])
-	aset(code, tmp, tmp2)
+for j = 0, 2 do
+	set(i, j)
+	set(v, math.pow(2, j + 1))
+	aset(a, i, v)
 end
 
-to(r)
-open()
-	aget(i, code, ip)
-	printCell(i)
-
-	to(r)
-close()
+for j = 2, 0, -1 do
+	set(i, j)
+	aget(v, a, i)
+	printCell(v)
+end
